420c5929982e39e3ac6c15e123d44f8c86ef85eb
Fabricio Washington <fabriciouitec@gmail.com>
2025-05-05 20:53:45 -0300

    ajustando fechar dialog

--- src/app/web/shared/components/dialog-convite/dialog-convite.component.ts
+++ src/app/web/shared/components/dialog-convite/dialog-convite.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-convite',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './dialog-convite.component.html',
   styleUrl: './dialog-convite.component.scss'
 })
-export class DialogConviteComponent {
+export class DialogConviteComponent implements OnInit {
 
+  constructor(private dialogRef: MatDialogRef<DialogConviteComponent>) {}
+
+  ngOnInit(): void {}
+
+  closeModal(): void {
+    this.dialogRef.close();
+  }
 }
